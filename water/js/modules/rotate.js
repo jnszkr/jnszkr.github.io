7dabff23501fb16b2abcf64c1e9de31ffc38d214
jnszkr <joszkar@gmail.com>
2014-10-05 18:58:12 +0200

    add rotate.js

---
+++ water/js/modules/rotate.js
@@ -0,0 +1,12 @@
+
+
+ko.bindingHandlers.rotate = {
+	init:function(element, valueAccessor, allBindings, viewModel, bindingContext){
+		var data = ko.unwrap(valueAccessor());
+
+	},
+	update: function(element, valueAccessor, allBindings, viewModel, bindingContext){
+		var data = ko.unwrap(valueAccessor());
+
+	}
+};
